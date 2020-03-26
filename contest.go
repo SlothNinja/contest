eeac6f3c8211ed6e7e117369ff3105f828275c6f
SlothNinja <slothninja2@gmail.com>
2020-03-26 16:45:46 -0400

    moved dsClient to server struct

--- contest.go
+++ contest.go
@@ -10,11 +10,17 @@ import (
 
 const kind = "Contest"
 
+type server struct {
+	*datastore.Client
+}
+
+func NewClient(dsClient *datastore.Client) server {
+	return server{Client: dsClient}
+}
+
 type Contests []*Contest
 type Contest struct {
-	c *gin.Context
-	// ID        int64          `gae:"$id"`
-	// Parent    *datastore.Key `gae:"$parent"`
+	c         *gin.Context
 	Key       *datastore.Key `datastore:"__key__"`
 	GameID    int64
 	Type      gtype.Type
@@ -54,8 +60,7 @@ type Places []ResultsMap
 
 func New(c *gin.Context, id int64, pk *datastore.Key, gid int64, t gtype.Type, r, rd, outcome float64) *Contest {
 	return &Contest{
-		Key: datastore.IDKey(kind, id, pk),
-		// Parent:  pk,
+		Key:     datastore.IDKey(kind, id, pk),
 		GameID:  gid,
 		Type:    t,
 		R:       r,
@@ -75,43 +80,14 @@ func GenContests(c *gin.Context, places Places) (cs Contests) {
 	return
 }
 
-//func (cs Contests) Keys() []*datastore.Key {
-//	ks := make([]*datastore.Key, len(cs))
-//	for i, c := range cs {
-//		ks[i] = c.Key
-//	}
-//	return ks
-//}
-
-//func (cs Contests) Save(c *restful.Context) error {
-//	keys := make([]*datastore.Key, len(cs))
-//	now := time.Now()
-//	for i, c := range cs {
-//		keys[i] = c.Key
-//		if c.CreatedAt.IsZero() {
-//			c.CreatedAt = now
-//		}
-//		c.UpdatedAt = now
-//	}
-//	return datastore.RunInTransaction(ctx, func(tc appengine.Context) error {
-//		_, err := gaelic.PutMulti(tc, keys, cs)
-//		return err
-//	}, nil)
-//}
-
-func UnappliedFor(c *gin.Context, ukey *datastore.Key, t gtype.Type) (Contests, error) {
-	dsClient, err := datastore.NewClient(c, "")
-	if err != nil {
-		return nil, err
-	}
-
+func (svr server) UnappliedFor(c *gin.Context, ukey *datastore.Key, t gtype.Type) (Contests, error) {
 	q := datastore.NewQuery(kind).
 		Ancestor(ukey).
 		Filter("Applied=", false).
 		Filter("Type=", int(t)).
 		KeysOnly()
 
-	ks, err := dsClient.GetAll(c, q, nil)
+	ks, err := svr.GetAll(c, q, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -126,7 +102,7 @@ func UnappliedFor(c *gin.Context, ukey *datastore.Key, t gtype.Type) (Contests, 
 		cs[i] = new(Contest)
 	}
 
-	err = dsClient.GetMulti(c, ks, cs)
+	err = svr.GetMulti(c, ks, cs)
 	if err != nil {
 		return nil, err
 	}
@@ -135,18 +111,13 @@ func UnappliedFor(c *gin.Context, ukey *datastore.Key, t gtype.Type) (Contests, 
 
 type ContestMap map[gtype.Type]Contests
 
-func Unapplied(c *gin.Context, ukey *datastore.Key) (ContestMap, error) {
-	dsClient, err := datastore.NewClient(c, "")
-	if err != nil {
-		return nil, err
-	}
-
+func (svr server) Unapplied(c *gin.Context, ukey *datastore.Key) (ContestMap, error) {
 	q := datastore.NewQuery(kind).
 		Ancestor(ukey).
 		Filter("Applied=", false).
 		KeysOnly()
 
-	ks, err := dsClient.GetAll(c, q, nil)
+	ks, err := svr.GetAll(c, q, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -159,12 +130,9 @@ func Unapplied(c *gin.Context, ukey *datastore.Key) (ContestMap, error) {
 	cs := make(Contests, length)
 	for i := range cs {
 		cs[i] = new(Contest)
-		// if ok := datastore.PopulateKey(cs[i], ks[i]); !ok {
-		// 	return nil, fmt.Errorf("Unable to populate contest with key.")
-		// }
 	}
 
-	err = dsClient.GetMulti(c, ks, cs)
+	err = svr.GetMulti(c, ks, cs)
 	if err != nil {
 		return nil, err
 	}
@@ -176,17 +144,3 @@ func Unapplied(c *gin.Context, ukey *datastore.Key) (ContestMap, error) {
 	}
 	return cm, nil
 }
-
-//func (c *Contest) Save(ch chan<- datastore.Property) error {
-//	// Time stamp
-//	t := time.Now()
-//	if c.CreatedAt.IsZero() {
-//		c.CreatedAt = t
-//	}
-//	c.UpdatedAt = t
-//	return datastore.SaveStruct(c, ch)
-//}
-//
-//func (c *Contest) Load(ch <-chan datastore.Property) error {
-//	return datastore.LoadStruct(c, ch)
-//}
